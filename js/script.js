004f7bcba2bd1ef38a8018280637e31d9e5e54af
davida330300 <davida330300@gmail.com>
2021-05-04 21:34:03 +0800

    test

--- js/script.js
+++ js/script.js
@@ -69,4 +69,33 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
-              
+
+let slideIndex = 1;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  console.log("wtf")
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
